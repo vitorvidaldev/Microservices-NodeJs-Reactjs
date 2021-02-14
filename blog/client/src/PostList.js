fb0343060c61b41577de71883113c9d752226ca6
Vitor <vitorvidal.dev@gmail.com>
2021-02-13 22:09:05 -0300

    Finished the basic client development

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
@@ -24,6 +26,8 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
