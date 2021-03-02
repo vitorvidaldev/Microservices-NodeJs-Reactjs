ac5fce0f1ae64900bb1356bd52dcaed88f1c1557
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-03-02 19:14:09 -0300

    Small update

--- blog/query/index.js
+++ blog/query/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Service to get all the data in the system (posts and comments)
 app.get("/posts", (req, res) => {});
 
 app.post("/events", (req, res) => {});
